797790d62f119ec10c6c6083cf667b6fe15d2cad
m0 <mo>
2024-02-10 14:17:59 +0100

    feat!: initial commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM alpine:3.17
+# Define build-time variables
+ARG POOF_PORT 
+ARG POOF_MODE
+ARG POOF_BANNER
+
+# Set the build-time variable as an environment variable
+ENV POOF_PORT=${POOF_PORT}
+ENV POOF_MODE=${POOF_MODE}
+ENV POOF_BANNER=${POOF_BANNER}
+
+# Copy files
+COPY ./src /home/poof/
+
+# Update apt repository and install dependencies
+RUN apk --no-cache -U add \
+    python3 \
+    py3-pip \
+    python3-dev && \
+    addgroup -g 2000 poof && \
+    adduser -S -s /bin/ash -u 2000 -D -g 2000 poof && \
+    chown poof:poof -R /home/poof/* 
+
+WORKDIR /home/poof
+USER poof:poof
+
+CMD python3 PortSpoof.py -p 65100 -m "$POOF_MODE" -b "$POOF_BANNER" -i "$POOF_PORT"
+
+
---
+++ src/PortSpoof.py
@@ -0,0 +1,115 @@
+import socket, logging, sys, argparse
+
+class PortSpoof:
+    """
+    This class is designed to spoof banners to clients trying to connect to a port.
+    
+    It should be able to work both in "banner" and in "http-header" mode.
+    """
+
+    def __init__(self, mode:str, banner_header:str, ip:str, port:int, logger:logging.Logger) -> None:
+        """
+        This method is the constructor for the PortSpoof class.
+        It is mainly used to set the mode to "banner" or "http-header", according with the appropriate parameter.
+
+        Regarding the `banner_header`-parameter:
+        here it is expected that the user passes a string only containing the banner or http-header value that is seen by nmap. (so no `\r\n` at the end or `HTTP/1.1 200 OK` at the beginning)
+
+        :param mode: The mode to use. Should be EITHER "banner" or "http-header".
+        :type mode: str
+        :param banner_header: The banner or header to send to the client.
+        :type banner_header: str
+        :param ip: The IP address to listen on.
+        :type ip: str
+        :param port: The port to listen on.
+        :type port: int
+        :param logger: the logger to use.
+        :type logger: Logger
+        :return: None
+        """
+
+        if mode in ["banner", "http-header"]:
+            self.mode = mode
+        else:
+            raise ValueError(f"Invalid mode: {mode}")
+        
+        self.banner_header = banner_header
+        self.logger = logger
+        self.ip = ip
+        self.port = port
+
+
+    def spoof(self) -> None:
+        """
+        Use this method to spoof an HTTP header or a banner to a client trying to connect to a port.
+
+        :return: None
+        """
+
+        socket_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket_listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        socket_listener.bind((self.ip, self.port))
+        socket_listener.listen(5)
+        self.logger.info(f"Listening on {self.ip}:{self.port}")
+
+        while True:
+            try:
+                client, adress = socket_listener.accept()
+                data = client.recv(1024)
+
+                if self.mode == "banner":
+                    client.send(bytes(f"{self.banner_header}\r\n\r\n", 'utf-8'))
+                
+                elif self.mode == "http-header":
+                    if data and self.is_valid_http_request(data.decode('utf-8')):
+                        client.send(bytes(f"HTTP/1.1 200 OK\r\nServer: {self.banner_header}\r\n\r\n", 'utf-8'))
+                    
+                client.close()
+            
+            except Exception as e:
+                self.logger.debug(f"Error: {e}")
+                continue
+    
+    def is_valid_http_request(self, request_line:str) -> bool:
+        """
+        This method is used to check if a given string is a valid HTTP request.
+
+        :param request_line: The string to check.
+        :type request_line: str
+        :return: True if the string is a valid HTTP request, False otherwise.
+        :rtype: bool
+        """
+
+        parts = request_line.split(' ')
+        if len(parts) != 3:
+            return False
+
+        method, _, version = parts
+        valid_methods = ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"]
+        if method not in valid_methods:
+            return False
+
+        if not version.startswith("HTTP/"):
+            return False
+
+        return True
+    
+
+if __name__ == '__main__':
+    logger = logging.Logger("portspoof")
+    logger.setLevel(logging.DEBUG)  # Set the logging level as needed
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    parser = argparse.ArgumentParser(description="Process some integers.")
+
+    # Add arguments
+    parser.add_argument('-p', '--port', type=int, required=True, help='The port to listen on')
+    parser.add_argument('-m', '--mode', type=str, required=True, choices=['banner', 'http-header'], help='The mode you want to run the script in')
+    parser.add_argument('-b', '--banner', type=str, required=True, help='The banner or http-header to send to the client')
+    parser.add_argument('-i', '--info_port', type=str, required=True, help='The actual outside port of the service for logging purposes')
+    args = parser.parse_args()
+    ps = PortSpoof(ip="0.0.0.0", port=args.port, banner_header=args.banner, mode=args.mode, logger=logger)
+    ps.spoof()
